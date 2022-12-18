6775314b2f1f1e1a5651e0651d901604620193c1
Baffi <lucasrenatobaffi@hotmail.com>
2022-12-18 20:23:41 -0300

    logica somatoria dos produtos

--- js/filter.js
+++ js/filter.js
@@ -1,32 +1,49 @@
-const filtroTodos = () => {
-    const button = document.getElementById('todos')  
-    button.addEventListener('click', () => {
-        renderProdcts(produtos)
-    })
-    
-}
-filtroTodos()
 
+// **************Hortfruti********************
 let filtroHortfrut = () => {
-    let Todos = produtos.filter((produto) => produto.secao === "Hortifruti")
-    renderProdcts(Todos);   
-  }
+  var sum = 0;
+  let Todos = produtos.filter((produto) => produto.secao === "Hortifruti")
+  renderProdcts(Todos);
+}
 
-  let buttonHortifrut = document.getElementsByClassName("button")[1]
-  buttonHortifrut.addEventListener("click", (filtroHortfrut))
+let buttonHortifrut = document.getElementsByClassName("button")[1]
+buttonHortifrut.addEventListener("click", () => {
+  filtroHortfrut(sumHorti())
+},)
 
-  let filtroPanificadora = () => {
-    let Todos = produtos.filter((produto) => produto.secao === "Panificadora")
-    renderProdcts(Todos);   
-  }
 
-  let buttonPanificadora = document.getElementsByClassName("button")[2]
-  buttonPanificadora.addEventListener("click", (filtroPanificadora))
+// **************panificadora********************
+let filtroPanificadora = () => {
+  let Todos = produtos.filter((produto) => produto.secao === "Panificadora")
+  renderProdcts(Todos);
+}
 
+let buttonPanificadora = document.getElementsByClassName("button")[2]
+buttonPanificadora.addEventListener("click", () => {
+  filtroPanificadora(sumPani())
+})
+
+
+// **************laticinios********************
 let filtroLaticinios = () => {
-    let Todos = produtos.filter((produto) => produto.secao === "Laticinio")
-    renderProdcts(Todos);   
-  }
+  let Todos = produtos.filter((produto) => produto.secao === "Laticinio")
+  renderProdcts(Todos);
+}
 
-  let buttonLaticinios = document.getElementsByClassName("button")[3]
-  buttonLaticinios.addEventListener("click", (filtroLaticinios))
+let buttonLaticinios = document.getElementsByClassName("button")[3]
+buttonLaticinios.addEventListener("click", () => {
+  filtroLaticinios(sumLat())
+})
+
+// ******************todos*********************
+
+
+const filtroTodos = () => {
+  let Todos = produtos.filter((produto) => produto.secao)
+  renderProdcts(Todos);
+}
+let buttonTod = document.getElementsByClassName("button")[0]
+buttonTod.addEventListener("click", () => {
+  filtroTodos(sumTodos())
+})
+
---
+++ js/search.js
@@ -0,0 +1,11 @@
+let inputSearch = document.getElementById('inputSearchProducts')
+let buttonLupa = document.querySelector('.lupa')
+console.log(buttonLupa);
+
+buttonLupa.addEventListener('click', () => {
+    
+})
+
+const valueInput = inputSearch.value
+
+console.log(valueInput);
